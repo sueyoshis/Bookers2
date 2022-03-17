553a6ca988315e1cd39928515ada29ada420c369
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-17 11:50:59 +0000

    [Add]本の新規投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get '/home/about' => 'homes#about', as:'about'
+  resources :books, only: [:index, :create, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
