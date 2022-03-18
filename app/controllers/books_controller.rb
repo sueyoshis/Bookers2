3534ea466f717937ba971701b2e9b05da129401c
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-18 08:55:09 +0000

    [Add]本の投稿内容編集機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id) #本の詳細へ遷移
   end
 
   def show
@@ -17,6 +17,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find (params[:id])
+    if @book.user == current_user #ログインユーザの時editのビューを表示
+      render :edit
+    else
+      redirect_to books_path #それ以外の時は本の一覧へ遷移
+    end
+  end
+
+  def update
+    @book = Book.find (params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id) #本の詳細へ遷移
   end
 
   private
