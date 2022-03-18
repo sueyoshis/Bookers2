3b64336b5d1f0369822f06b5223a6e183afa0f16
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-18 05:48:10 +0000

    [Update]ユーザの一覧・詳細にユーザアイコンを表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'defaule_image.jpg', content_type: 'image.jpeg')
     end
-    profile_image.variant(resize_to limit: [width, height]).prosessed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
