a7aaaf30e44dfb406faddc17566b7bf2daf7581f
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-19 06:02:50 +0000

    [Update]ユーザ情報更新機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    user = User.find (params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
